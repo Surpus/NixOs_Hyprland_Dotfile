143062f5764264c2101db871ceb9cf4f89e3d66e
surpur <gregtechemail@proton.me>
2024-11-24 20:38:29 +0700

    added icon folder and patch script

---
+++ patch.sh
@@ -0,0 +1,5 @@
+$CURRDIR = $(pwd)
+cp -r $CURRDIR/hypr ~/.config/
+cp -r $CURRDIR/waybar ~/.config/
+cp -r $CURRDIR/icons ~/.icons
+cp $CURRDIR/bashrc ~/.bashrc
--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@ cp /etc/nixos/configuration.nix ~/dotfiles/nixos/
 cp ~/.bashrc ~/dotfiles/bashrc
 cp -r ~/.config/hypr ~/dotfiles/
 cp -r ~/.config/waybar ~/dotfiles/
-cp -r ~/.icons ~/dotfiles/
+cp -r ~/.icons ~/dotfiles/icons
